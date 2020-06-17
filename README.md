ff8ff065790eab072cd5c49fbd58d3889eaf23ff
c0h2r <liaollib@gmail.com>
2020-06-17 17:29:26 +0300

    Removed original files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# dwm-6.2-cpp
-Dwm ported with c++11 forked from dwm-cpp by slankdev and dwm 6.2
--- slankdev/util.h
+++
@@ -1,6 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-
-#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
-
-void die(const char *errstr);
-void *ecalloc(size_t, size_t);
